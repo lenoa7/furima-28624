2906403ae54921dda4781b9dea4802903bc5e776
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2020-12-17 20:13:01 +0900

    作業中

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email , presence: true , format: { with: VALID_EMAIL_REGEX }
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :encrypted_password ,presence: true , confirmation: true , length: {minimum: 6},format: { with: VALID_PASSWORD_REGEX }
+  validates :password ,presence: true , confirmation: true , length: {minimum: 6},format: { with: VALID_PASSWORD_REGEX }
 end
