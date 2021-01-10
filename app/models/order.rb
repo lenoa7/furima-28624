3d1cd066b6cdaa6ef880624e5166d79a90f3b74b
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-10 23:10:01 +0900

    orders indexルーティングの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ApplicationRecord
+
+  VALID_POSTCODE_REGEX = ^\d{3}-\d{4}$
+  VALID_PHONE_REGEX = /^0\d{10,11}$/
+
+  with_options presence: true do 
+    validates :post_code, format: { with: VALID_POSTCODE_REGEX }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: VALID_PHONE_REGEX }
+    validates :prefecture_id
+  end
 end
+
