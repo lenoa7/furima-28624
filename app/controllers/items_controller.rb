ee456f6f7f273289cc37401eed209cc8ec221a51
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-10 11:43:16 +0900

    editページリダイレクト処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
+  before_action :correct_user, only: [:edit, :update]
   def index
     @item = Item.all.order(created_at: :desc).includes(:user)
   end
@@ -25,8 +26,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:introduce,:category_id,:condition_id,:delivery_fee_id,:delivery_days_id,:price,:prefecture_id).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    redirect_to root_path if current_user.id != @item.user_id
   end
 end
