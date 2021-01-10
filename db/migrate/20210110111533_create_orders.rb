b961a6d692ee915d557654e0519ca2efc37ce34e
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-10 21:43:52 +0900

    orderモデルのmigrate

--- db/migrate/20210110111533_create_orders.rb
+++ db/migrate/20210110111533_create_orders.rb
@@ -1,8 +1,17 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.integer :post_code, null: false
+      t.string  :city, null: false
+      t.string  :house_number, null: false
+      t.string  :building_name
+      t.integer :phone_number, null: false
+      t.integer :prefecture_id, null: false
+      t.bigint  :user_id
+      t.bigint  :item_id
       t.timestamps
     end
+    add_foreign_key :orders, :users
+    add_foreign_key :orders, :items
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_052211) do
+ActiveRecord::Schema.define(version: 2021_01_10_111533) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,21 @@ ActiveRecord::Schema.define(version: 2021_01_03_052211) do
     t.index ["user_id"], name: "fk_rails_d4b6334db2"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "post_code", null: false
+    t.string "city", null: false
+    t.string "house_number", null: false
+    t.string "building_name"
+    t.integer "phone_number", null: false
+    t.integer "prefecture_id", null: false
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "fk_rails_fc971cd604"
+    t.index ["user_id"], name: "fk_rails_f868b47f6a"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -68,4 +83,6 @@ ActiveRecord::Schema.define(version: 2021_01_03_052211) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
