e3d2029bcc61aa25efe97f48938e3930e6d13f12
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-12 23:24:25 +0900

    バリデーションの設定

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -2,14 +2,21 @@ class BuyForm
   include ActiveModel::Model
   include ActiveModel::AttributeMethods
 
-  attr_accessor :user_id, :item_id, :order_id, :post_code, :city, :house_number, :building_name, :phone_number, :prefecture_id
+  attr_accessor :user_id, :item_id, :order_id, :post_code, :city, :house_number, :building_name, :phone_number, :prefecture_id, :card_number, :card_month, :card_year, :security_code
+
+  VALID_POST_REGEX = /\A\d{3}[-]\d{4}\z/
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/
 
   with_options presence: true do 
-    validates :post_code
+    validates :card_number
+    validates :card_year
+    validates :card_month
+    validates :security_code
+    validates :post_code, format: { with: VALID_POST_REGEX }
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" } 
     validates :city
     validates :house_number
-    validates :phone_number
-    validates :prefecture_id, numericality: { other_than: 1, message: "Select" } 
+    validates :phone_number, format: { with: VALID_PHONE_REGEX }
   end
 
   def buy_save
