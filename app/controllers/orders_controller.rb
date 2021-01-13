c23deaf7267b87eb1e1c8d2d56993b24b44e2d2f
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-13 09:01:07 +0900

    ordersコントローラーの整理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,14 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :item_find
+  before_action :correct_order_user
 
   def new
-    @item = Item.find(params[:id])
     @order = BuyForm.new
   end
 
   def create
     @order = BuyForm.new(order_params)
-    @item = Item.find(params[:id])
     if @order.valid? && @order.buy_save
        redirect_to root_path
     else
@@ -20,5 +19,13 @@ class OrdersController < ApplicationController
   private
   def order_params
     params.require(:buy_form).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:id])
+  end
+
+  def correct_order_user
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
