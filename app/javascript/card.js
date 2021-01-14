7b43fe736a5e6e224935cc6cdfa1156580583232
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-15 01:31:44 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  Payjp.setPublicKey("pk_test_29c07143792d831b45c7566f");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
