097b2bf25c4be5fe001a8dcc8c812e3f90710680
lenoa7 <junmen.tk.719.xyz@ezweb.ne.jp>
2021-01-15 10:50:33 +0900

    factories/buy_formの修正

--- spec/factories/buy_form.rb
+++ spec/factories/buy_form.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :buy_form do
     post_code             {"123-1234"}
-    prefecture_id         {"2"}
+    prefecture_id         {2}
     city                  {"大阪市"}
     house_number          {"大今里1-1-1"}
     phone_number          {"0612341234"}
